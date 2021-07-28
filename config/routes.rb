f2ac8a855b214960afffba69b70c19c54dbd29b6
Chisthoper Mella <chisthoper96@gmail.com>
2021-07-27 22:57:00 -0400

    Controller List

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'lists', to: 'lists#index'
+
+  get 'lists/new', to: 'lists#new', as: :new_list
+  post 'lists', to: 'lists#create'
+
+  get 'lists/:id', to: 'lists#show', as: :list
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
