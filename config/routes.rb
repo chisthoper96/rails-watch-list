3457f5f49b97b663e992c48b23e1acf71452491c
Chisthoper Mella <chisthoper96@gmail.com>
2021-07-29 14:42:43 -0400

    v.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'lists', to: 'lists#index'
-
-  get 'lists/new', to: 'lists#new', as: :new_list
-  post 'lists', to: 'lists#create'
-
-  get 'lists/:id', to: 'lists#show', as: :list
-
+  # root to: 'lists'
+  resources :lists, except: [:edit, :update, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
