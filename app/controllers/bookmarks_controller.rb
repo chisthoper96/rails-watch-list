ccf61b07a6c73a0ef2332ed4687e3cd16eca4f8f
Chisthoper Mella <chisthoper96@gmail.com>
2021-07-29 16:55:53 -0400

    arreglando el crate

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -24,6 +24,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
